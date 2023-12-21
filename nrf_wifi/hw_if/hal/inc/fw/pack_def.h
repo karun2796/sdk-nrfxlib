9ba202c8991b600714638e102e5c524d57261d5b
Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>
2023-12-21 17:18:44 +0530

    nrf_wifi: Update RPU patch to rev#cd3b53dc463
    UMAC:
            [SHEL-2067] : High DTIM current consumption due to UMAC sending
                          sleep request failures for 100mses.
            [SHEL-2181] : Change scan channel type to active in connected scan req.
            [SHEL-2328] : SAP: Unaligned structure causing write issues.
            [SHEL-2318] : Error case handling in umac_set_ifflags in case of
                          wrong interface name.
    
    LMAC:
            [SHEL-2165] : Add checksum to the patch binary header.
            [SHEL-2066] : Observed Long Wake Intervals When Station Is Idle.
            [SHEL-2301] : High DTIM power with Qualcomm AP.
            [SHEL-1937] : Power save characterisation in raw packet mode.
            [SHEL-2315] : LPM device does not poll on becaons with TIM_AID_1=1.
    
    Signed-off-by: Karun Kumar Eagalapati <karun.kumar@nordicsemi.no>

--- nrf_wifi/hw_if/hal/inc/fw/pack_def.h
+++ nrf_wifi/hw_if/hal/inc/fw/pack_def.h
@@ -12,5 +12,7 @@
 #define __PACK_DEF__
 
 #define __NRF_WIFI_PKD __attribute__((__packed__))
+#define __NRF_WIFI_ALIGN_4 __attribute__((__aligned__(4)))
+#define __NRF_WIFI_PKD_ALIGN_4 __attribute__((__packed__, __aligned__(4)))
 
 #endif
